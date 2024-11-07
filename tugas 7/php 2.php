0879d6e4e448f77436b20d48909cff09954ec1bc
fahrulii <muhammadaldinakbarfahruli@saintek.unipdu.ac.id>
2024-11-07 13:16:12 +0700

    tugas 7

---
+++ tugas 7/php 2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh List Dengan PHP</title>
+</head>
+<body>
+    <H2>Daftar Absensi Mahasiswa</H2>
+    <ol>
+        </php?
+        for ($i= 1; $i <= 1000; $i++) 
+        {
+          echo "<li>Nama Mahasiswa ke-$i</li>";
+        }
+        ?>
+    </ol>
+    
+</body>
+</html>
